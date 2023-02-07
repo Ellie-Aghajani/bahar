edc768c360958932ebafeafdfba29cfa62295686
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-07 11:28:12 -0800

    add font awesome 4.7.0 and bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
+import "font-awesome/css/font-awesome.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ client/src/services/fakePlantService.js
@@ -0,0 +1,96 @@
+import * as categoriesAPI from "./fakeCategoryService";
+
+const plants = [
+  {
+    _id: "5b21ca3eeb7f6fbccd471815",
+    title: "Snake Laurentii",
+    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Low Light Tolerant" },
+    numberInStock: 6,
+    dailySalesRate: 2.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471816",
+    title: "Micans",
+    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Low Light Tolerant" },
+    numberInStock: 5,
+    dailySalesRate: 2.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471817",
+    title: "Calathea",
+    category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Pet Friendly" },
+    numberInStock: 8,
+    dailySalesRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471819",
+    title: "Zz Plant",
+    category: { _id: "5b21ca3eeb7f6fbccd471814", name: "Hard to Kill" },
+    numberInStock: 7,
+    dailySalesRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47181a",
+    title: "Philodendron",
+    category: { _id: "5b21ca3eeb7f6fbccd471814", name: "Hard to Kill" },
+    numberInStock: 7,
+    dailySalesRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47181b",
+    title: "Fern",
+    category: { _id: "5b21ca3eeb7f6fbccd471814", name: "Hard to Kill" },
+    numberInStock: 7,
+    dailySalesRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47181e",
+    title: "Parlor Palm",
+    category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Pet Friendly" },
+    numberInStock: 7,
+    dailySalesRate: 4.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47181f",
+    title: "Peperomia",
+    category: { _id: "5b21ca3eeb7f6fbccd471820", name: "Pet Friendly" },
+    numberInStock: 4,
+    dailySalesRate: 3.5
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471821",
+    title: "Obtusifolia",
+    category: { _id: "5b21ca3eeb7f6fbccd471818", name: "Low Light Tolerant" },
+    numberInStock: 7,
+    dailySalesRate: 3.5
+  }
+];
+
+export function getPlants() {
+  return plants;
+}
+
+export function getPlant(id) {
+  return plants.find(m => m._id === id);
+}
+
+export function savePlant(plant) {
+  let plantInDb = plants.find(m => m._id === plant._id) || {};
+  plantInDb.name = plant.name;
+  plantInDb.category = categoriesAPI.categories.find(g => g._id === plant.categoryId);
+  plantInDb.numberInStock = plant.numberInStock;
+  plantInDb.dailySalesRate = plant.dailySalesRate;
+
+  if (!plantInDb._id) {
+    plantInDb._id = Date.now();
+    plants.push(plantInDb);
+  }
+
+  return plantInDb;
+}
+
+export function deletePlant(id) {
+  let plantInDb = plants.find(m => m._id === id);
+  plants.splice(plants.indexOf(plantInDb), 1);
+  return plantInDb;
+}
