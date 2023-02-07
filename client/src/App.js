db9197c91e571cdfcfe8cdeeb3992447914fedc0
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-07 11:26:49 -0800

    plsnts component to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import React, { Component} from "react";
+import Plants from'./components/plants';
 import Counter from './components/Counter';
 
-function App() {
-  return (
-    <div>
-      
-      <Counter />
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <main className='container'>
+        <Plants />
+      </main>
+    );
+  }
 }
+
+
 
 export default App;
