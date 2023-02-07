ead0b9f7ed2dc3f9065e3302b02dd371d40ef8b2
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-06 22:32:32 -0800

    add eventHandler

--- client/src/components/Counter.jsx
+++ client/src/components/Counter.jsx
@@ -6,22 +6,24 @@ class  Counter extends Component {
     state = {
         count: 0,
         
+
     };
   
     handleIncrement = () => {
-        console.log("increment clicked", this);
-    }
+        this.setState({count: this.state.count + 1})
+    };
     
-       
+   
 
     render() { 
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
+                <button 
+                 onClick={()=> this.handleIncrement({id:1})}
+                 className="btn btn-secondary btn-sm">Increment
+                 </button>
+               
             </div>
         );
     }
