a802a0a1b2d2e5c24ff9076470f6260c31c884b0
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-09 01:05:33 -0800

    refine decrement section

--- client/src/components/Counter.jsx
+++ client/src/components/Counter.jsx
@@ -4,18 +4,24 @@ class Counter extends Component {
     render() { 
     return (
     <div>
-    <span className={this.getBadgeClasses()}>     {this.formatCount()}</span>
-    <button onClick = {() => this.props.onIncrement(this.props.counter)}
-    className="btn btn-secondary btn-sm">
-    Increment
-    </button>
-    <button onClick = {(counterId) =>this.props.onDelete(this.props.counter.id)} 
-    className="btn btn-danger btn-sm">
-    Delete
-    </button>
+        <span className={this.getBadgeClasses()}>     {this.formatCount()}</span>
+        <button onClick = {() => this.props.onIncrement(this.props.counter)}
+        className="btn btn-secondary btn-sm">
+        +
+        </button>
+
+        <button onClick = {() => this.props.onDecrement(this.props.counter)}
+        className="btn btn-secondary btn-sm">
+        -
+        </button>
+
+        <button onClick = {(counterId) =>this.props.onDelete(this.props.counter.id)} 
+        className="btn btn-danger btn-sm">
+        Delete
+        </button>
     </div>
-    );
-    }
+        );
+        }
     getBadgeClasses(){
         let classes = "badge m-2 badge-";
         classes += this.props.counter.value === 0 ? "warning" : "primary";
