0542d48ba086196d870f04c28ce358b7295d477f
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-09 01:06:35 -0800

    include onDecrement

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,14 @@ class App extends Component {
      this.setState({ counters });
  };
 
+ handleDecrement = counter =>{
+  const counters = [...this.state.counters];
+  const index = counters.indexOf(counter);
+  counters[index] = { ...counter };
+  counters[index].value--;
+  this.setState({ counters });
+};
+
  handleReset = () => {
     const counters = this.state.counters.map(c => {
         c.value = 0;
@@ -43,6 +51,7 @@ class App extends Component {
             counters={this.state.counters}
             onReset={this.handleReset} 
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
             onDelete={this.handleDelete} />
             <Plants />
         </main>
