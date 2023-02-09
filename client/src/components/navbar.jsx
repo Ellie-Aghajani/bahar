eecb89f80852e7bdfe71f5392ecdd9607926bb62
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-08 19:52:20 -0800

    navbar made stateless

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
-class Navbar extends Component {
-    
-    render() {
-        return (
+
+
+const Navbar = (props) => {
+     return (
             <nav className='navbar navbar-light bg-light'>
                 <a className='navbar-brand' href="#">
                 Navbar {""} 
                 <span className ="badge badge-pill badge-secondary">
-                    {this.props.totalCounters}</span>
+                    {props.totalCounters}</span>
                 </a>
             </nav>
         );
+      
     }
-}
+
 export default Navbar;
