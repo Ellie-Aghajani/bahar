b2a77e1328eba4af56eccb3c860150a081f4c628
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-08 19:57:03 -0800

    navbar made stateless

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 
 
-const Navbar = (props) => {
+const Navbar = ({totalCounters}) => {
      return (
             <nav className='navbar navbar-light bg-light'>
                 <a className='navbar-brand' href="#">
                 Navbar {""} 
                 <span className ="badge badge-pill badge-secondary">
-                    {props.totalCounters}</span>
+                    {totalCounters}</span>
                 </a>
             </nav>
         );
