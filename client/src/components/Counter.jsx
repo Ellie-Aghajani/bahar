6243c10d2d14ebe29b9a61522f59f840aa559f13
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-09 12:36:29 -0800

    decrement button disabled for zero figure

--- client/src/components/Counter.jsx
+++ client/src/components/Counter.jsx
@@ -3,15 +3,20 @@ import React, { Component } from 'react'
 class Counter extends Component {      
     render() { 
     return (
-    <div>
+    <div className='row'>
+        <div className='col-1'>
         <span className={this.getBadgeClasses()}>     {this.formatCount()}</span>
+        </div>
+        <div className='col'>
         <button onClick = {() => this.props.onIncrement(this.props.counter)}
         className="btn btn-secondary btn-sm">
         +
         </button>
 
-        <button onClick = {() => this.props.onDecrement(this.props.counter)}
-        className="btn btn-secondary btn-sm">
+        <button 
+        onClick = {() => this.props.onDecrement(this.props.counter)}
+        className="btn btn-secondary btn-sm m-2"
+        disabled={this.props.counter.value === 0 ? 'disabled' : ''}>
         -
         </button>
 
@@ -19,6 +24,9 @@ class Counter extends Component {
         className="btn btn-danger btn-sm">
         Delete
         </button>
+        </div>
+        
+
     </div>
         );
         }
