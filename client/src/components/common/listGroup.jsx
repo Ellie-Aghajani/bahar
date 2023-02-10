fe6a0fb74be4e96863b1aef3832e8609661df1d7
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-10 10:46:21 -0800

    file refactored

--- client/src/components/common/listGroup.jsx
+++ client/src/components/common/listGroup.jsx
@@ -11,12 +11,8 @@ const ListGroup = ({
     <ul className="list-group">
       {items.map(item => (
         <li
-          onClick={() => onItemSelect(item)}
           key={item[valueProperty]}
-          className={
-            item === selectedItem ? "list-group-item active" : "list-group-item"
-          }
-        >
+          className= "list-group-item">
           {item[textProperty]}
         </li>
       ))}
@@ -24,9 +20,6 @@ const ListGroup = ({
   );
 };
 
-ListGroup.defaultProps = {
-  textProperty: "name",
-  valueProperty: "_id"
-};
+
 
 export default ListGroup;
