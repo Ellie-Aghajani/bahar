907c4a59a7120447cbb16277d5f94e5d06d31467
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-09 16:43:44 -0800

    add input proprties to pagination

--- client/src/components/plants.jsx
+++ client/src/components/plants.jsx
@@ -1,13 +1,27 @@
 import React, {Component} from "react";
 import {getPlants} from "../services/fakePlantService";
+import Like from "./common/like"
+import Pagination from "./common/pagination"
 
 class Plants extends Component {
     state = {
-        plants:getPlants()
+        plants:getPlants(),
+        pageSize:4
     };
     handleDelete = (plant) => {
         const plants = this.state.plants.filter(m =>m._id !== plant._id);
         this.setState({plants});
+    }
+    handleLike = (plant)=>{
+        const plants = [...this.state.plants];
+        const index = plants.indexOf(plant);
+        plants[index]= {...plants[index]};
+        plants[index].liked = !plants[index].liked;
+        this.setState({plants});
+
+    };
+    handlePageChange= page => {
+
     }
     render() {
         const {length:count} = this.state.plants;
@@ -23,7 +37,7 @@ class Plants extends Component {
                         <th>Category</th>
                         <th>Stock</th>
                         <th>Rate</th>
-                        <th>Delete</th>
+                        
                     </tr>
                 </thead>
                 <tbody>
@@ -33,7 +47,15 @@ class Plants extends Component {
                         <td>{plant.category.name}</td>
                         <td>{plant.numberInStock}</td>
                         <td>{plant.dailySalesRate}</td>
-                        <td><button onClick={()=> this.handleDelete(plant)} className= "btn btn-danger btn-sm">Delete</button>
+                        <td>
+                            <Like liked={plant.liked} onClick={() => this.handleLike(plant) }/>
+                        </td>
+                        <td>
+                            <button 
+                            onClick={()=> this.handleDelete(plant)} 
+                            className= "btn btn-danger btn-sm">
+                                Delete
+                            </button>
                         </td>
                     </tr>
                     ))}
@@ -41,6 +63,10 @@ class Plants extends Component {
                 </tbody>
                 
             </table>
+            <Pagination 
+                itemsCount= {count} 
+                pageSize={this.state.pageSize} 
+                onPage={this.handlePageChange}/> 
             </React.Fragment>
             
 
