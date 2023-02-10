bc8cc6d229d2653d4cb04c14eaf3950676237654
Ellie-Aghajani <z.aqajani@gmail.com>
2023-02-09 23:47:18 -0800

    conncted to mongodb

--- server/index.js
+++ server/index.js
@@ -1,22 +1,23 @@
-const morgan = require('morgan');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const categories = require('./routs/categories')
-const home = require('./routs/home')
-const logger = require('./middleware/logger')
+const categories = require('./routes/categories')
+const home = require('./routes/home')
 const Joi = require('joi');
 const port = process.env.PORT|| 2017;
 
-app.set('view engine', 'pug');
+mongoose.connect('mongodb://localhost/bahar')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+
 app.use(express.json());
 app.use('/api/categories', categories);
 app.use('/', home);
 app.use(express.urlencoded ({ extended: true })) ;
 app.use(express.static('public'));
 
-if (app.get('env') === 'development') {
-    app.use(morgan('dev'));
-};
+
 
 app.listen(port, ()=>{
     console.log(`App is listening on port ${port} 😎👌`);
