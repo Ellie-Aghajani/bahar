f1b79d3d7453750c8a8a150edbe5bd222a72f698
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-10 23:00:32 -0800

    code refactored

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import Plants from "./components/movies";
+import Plants from "./components/plants";
 import Form from "./components/form";
-import Customers from "./components/customers";
+import Customers from "./components/customer";
 import Purchase from "./components/purchase";
 import NotFound from "./components/notFound";
 import Navbar from "./components/navbar";
@@ -12,12 +12,12 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <Navbar />
         <main className="container">
           <Switch>
-            <Route path="/plants/:id" component={PlantForm} />
+            <Route path="/plants/:id" component={Form} />
             <Route path="/plants" component={Plants} />
-            <Route path="/customers" component={Customers} />
+            <Route path="/customer" component={Customers} />
             <Route path="/purchase" component={Purchase} />
             <Route path="/not-found" component={NotFound} />
             <Redirect from="/" exact to="/plants" />
