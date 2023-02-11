15ec390999417816ea879394d3711d36534e7a9a
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-10 23:21:50 -0800

    file created and state assigned

---
+++ client/src/components/loginForm.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Form from "./common/form";
+
+class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {}
+  };
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
