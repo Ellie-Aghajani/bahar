4caf8d093b8badcbda3f486b5290b1280be4eeee
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-11 01:14:37 -0800

    add schema

--- client/src/components/form.jsx
+++ client/src/components/form.jsx
@@ -4,13 +4,33 @@ class Form extends Form {
   state = {
     data: {
       title: "",
-      genreId: "",
+      categoryId: "",
       numberInStock: "",
-      dailyRentalRate: ""
+     
     },
-    genres: [],
+    category: [],
     errors: {}
   };
+  schema = {
+    _id: Joi.string(),
+    title: Joi.string()
+      .required()
+      .label("Title"),
+    genreId: Joi.string()
+      .required()
+      .label("Genre"),
+    numberInStock: Joi.number()
+      .required()
+      .min(0)
+      .max(100)
+      .label("Number in Stock"),
+    dailyRentalRate: Joi.number()
+      .required()
+      .min(0)
+      .max(10)
+      .label("Daily Rental Rate")
+  };
+
 };
 
 export default Form;
