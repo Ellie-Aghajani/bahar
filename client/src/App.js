f809f039b5745a59e3112b63be5f61dc5493bb48
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-10 20:52:59 -0800

    add route path

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import plants from "./components/plants";
+import Plants from "./components/movies";
+import PlantForm from "./components/plantForm";
 import Customers from "./components/customers";
-
+import Purchase from "./components/Purchase";
+import NotFound from "./components/notFound";
+import Navbar from "./components/navbar";
 import "./App.css";
 
 class App extends Component {
@@ -15,7 +18,7 @@ class App extends Component {
             <Route path="/plants/:id" component={PlantForm} />
             <Route path="/plants" component={Plants} />
             <Route path="/customers" component={Customers} />
-            <Route path="/purchase" component={purchase} />
+            <Route path="/purchase" component={Purchase} />
             <Route path="/not-found" component={NotFound} />
             <Redirect from="/" exact to="/plants" />
             <Redirect to="/not-found" />
