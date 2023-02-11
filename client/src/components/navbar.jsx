f8630c701c7d97e331e86b41b5811aec4ae24bb9
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-10 23:03:59 -0800

    add items to the navbar (refactoring the routes)

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -20,8 +20,8 @@ const NavBar = () => {
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          <NavLink className="nav-item nav-link" to="/movies">
-            Movies
+          <NavLink className="nav-item nav-link" to="/plants">
+            plants
           </NavLink>
           <NavLink className="nav-item nav-link" to="/customers">
             Customers
