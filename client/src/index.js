8f62beb3dffc5599aaf7503afa49cdeadd74d1f7
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-10 23:01:44 -0800

    code refactored

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.css";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-       <BrowserRouter>
-        <App />
-        
-       </BrowserRouter>
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
