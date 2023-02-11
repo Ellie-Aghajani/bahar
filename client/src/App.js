04355750171e1899d6d726b4099f147feebda870
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-02-10 20:35:02 -0800

    add route path

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,29 @@
-import './App.css';
-import React, { Component} from "react";
-import Plants from'./components/plants';
-import Counters from './components/counters';
-import Navbar from "./components/navbar";
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import plants from "./components/plants";
+import Customers from "./components/customers";
+
+import "./App.css";
 
 class App extends Component {
-  state = { 
-    counters : [
-        { id: 1, value: 4},
-        { id: 2, value: 0},
-        { id: 3, value: 0},
-        { id: 4, value: 0}
-    ]
- }
-
- handleIncrement = counter =>{
-     const counters = [...this.state.counters];
-     const index = counters.indexOf(counter);
-     counters[index] = { ...counter };
-     counters[index].value++;
-     this.setState({ counters });
- };
-
- handleDecrement = counter =>{
-  const counters = [...this.state.counters];
-  const index = counters.indexOf(counter);
-  counters[index] = { ...counter };
-  counters[index].value--;
-  this.setState({ counters });
-};
-
- handleReset = () => {
-    const counters = this.state.counters.map(c => {
-        c.value = 0;
-        return c;
-    });
-    this.setState({ counters: counters });
-}
-
- handleDelete = (counterId) =>{
-     const counters = this.state.counters.filter(c => c.id !== counterId)
-     this.setState({counters});
- }
-  render(){
+  render() {
     return (
       <React.Fragment>
-        <Navbar totalCounters={this.state.counters.filter(c=> c.value > 0).length}/>
-        <main className='container'>
-            <Counters 
-            counters={this.state.counters}
-            onReset={this.handleReset} 
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete} />
-            <Plants />
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/plants/:id" component={PlantForm} />
+            <Route path="/plants" component={Plants} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/purchase" component={purchase} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/plants" />
+            <Redirect to="/not-found" />
+          </Switch>
         </main>
       </React.Fragment>
     );
   }
 }
-
-
 
 export default App;
