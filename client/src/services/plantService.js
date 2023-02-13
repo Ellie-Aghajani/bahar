f2380c7e3a03e499e0dbe25d002e1069d62963b5
Ellie-Aghajani <z.aqajani@gmail.com>
2023-02-13 00:57:35 -0800

    removed brackets in import

--- client/src/services/plantService.js
+++ client/src/services/plantService.js
@@ -1,23 +1,30 @@
-import http from './httpService';
+import http from "./httpService";
+// import apiUrl from "../config.json";
+
 const apiEndpoint = "http://localhost:2017/api/plants";
 
-
-
+function plantUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
 
 export function getPlants() {
-    return http.get(apiEndpoint);
+  return http.get(apiEndpoint);
 }
 
-export function getPlant(plantId){
-    return http.get(apiEndpoint + '/' + plantId);
+export function getPlant(plantId) {
+  return http.get(plantUrl(plantId));
 }
 
-export function savePlant(plant){
+export function savePlant(plant) {
+  if (plant._id) {
+    const body = { ...plant };
+    delete body._id;
+    return http.put(plantUrl(plant._id), body);
+  }
 
+  return http.post(apiEndpoint, plant);
 }
 
-
-
-export function deletePlant(plantId){
-    return http.delete(apiEndpoint + "/" + plantId);
+export function deletePlant(plantId) {
+  return http.delete(plantUrl(plantId));
 }
